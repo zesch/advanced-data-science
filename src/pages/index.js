fddf2f8885e323dc242151d19b80d834215adbde
giggity-hub <imigstefan@gmail.com>
2021-07-21 18:59:51 +0200

    make 'Setup'-button info color

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ function HomepageHeader() {
                   Lectures
                 </Link>
                 <Link
-                  className="button button--secondary button--lg margin-bottom--md"
+                  className="button button--info button--lg margin-bottom--md"
                   to="/setup/">
                   Setup
                 </Link>
