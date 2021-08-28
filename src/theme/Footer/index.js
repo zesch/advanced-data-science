837882ba86391808329084113ebe67211b54f938
giggity-hub <imigstefan@gmail.com>
2021-08-28 14:16:32 +0200

    customized swizzled footer

---
+++ src/theme/Footer/index.js
@@ -0,0 +1,130 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import {useThemeConfig} from '@docusaurus/theme-common';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+import ThemedImage from '@theme/ThemedImage';
+
+function FooterLink({to, href, label, prependBaseUrlToHref, ...props}) {
+  const toUrl = useBaseUrl(to);
+  const normalizedHref = useBaseUrl(href, {
+    forcePrependBaseUrl: true,
+  });
+  return (
+    <Link
+      className="footer__link-item"
+      {...(href
+        ? {
+            href: prependBaseUrlToHref ? normalizedHref : href,
+          }
+        : {
+            to: toUrl,
+          })}
+      {...props}>
+      {label}
+    </Link>
+  );
+}
+
+const FooterLogo = ({sources, alt}) => (
+  <ThemedImage className="footer__logo" alt={alt} sources={sources} />
+);
+
+function Footer() {
+  const {footer} = useThemeConfig();
+  const {copyright, links = [], logo = {}} = footer || {};
+  const sources = {
+    light: useBaseUrl(logo.src),
+    dark: useBaseUrl(logo.srcDark || logo.src),
+  };
+
+  const githubSources = {
+    light: useBaseUrl('img/GitHub-Mark-Light-64px.png'),
+    dark: useBaseUrl('img/GitHub-Mark-Light-64px.png')
+  }
+
+  if (!footer) {
+    return null;
+  }
+
+  return (
+    <footer
+      className={clsx('footer', {
+        'footer--dark': footer.style === 'dark',
+      })}>
+      <div className="container">
+        {links && links.length > 0 && (
+          <div className="row footer__links">
+            {links.map((linkItem, i) => (
+              <div key={i} className="col footer__col">
+                {linkItem.title != null ? (
+                  <div className="footer__title">{linkItem.title}</div>
+                ) : null}
+                {linkItem.items != null &&
+                Array.isArray(linkItem.items) &&
+                linkItem.items.length > 0 ? (
+                  <ul className="footer__items">
+                    {linkItem.items.map((item, key) =>
+                      item.html ? (
+                        <li
+                          key={key}
+                          className="footer__item" // Developer provided the HTML, so assume it's safe.
+                          // eslint-disable-next-line react/no-danger
+                          dangerouslySetInnerHTML={{
+                            __html: item.html,
+                          }}
+                        />
+                      ) : (
+                        <li key={item.href || item.to} className="footer__item">
+                          <FooterLink {...item} />
+                        </li>
+                      ),
+                    )}
+                  </ul>
+                ) : null}
+              </div>
+            ))}
+          </div>
+        )}
+        {(logo || copyright) && (
+          <div className={clsx("footer__bottom", "text--center", styles.footer__bottom)}>
+            {logo && (logo.src || logo.srcDark) && (
+              <div className={styles.footer__bottom_el}>
+                {logo.href ? (
+                  <Link href={logo.href} className={styles.footerLogoLink}>
+                    <FooterLogo alt={logo.alt} sources={sources} />
+                  </Link>
+                ) : (
+                  <FooterLogo alt={logo.alt} sources={sources} />
+                )}
+              </div>
+            )}
+            <div className={styles.footer__bottom_el}>
+              <Link href='https://github.com/zesch/practical-data-science' className={styles.footerLogoLink}>
+                <FooterLogo alt="shheeeesh" sources={githubSources} />
+              </Link>
+            </div>
+            {copyright ? (
+              <div
+                className={clsx("footer__copyright", styles.footer__bottom_el, styles.footer__copyright)} // Developer provided the HTML, so assume it's safe.
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{
+                  __html: copyright,
+                }}
+              />
+            ) : null}
+          </div>
+        )}
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
