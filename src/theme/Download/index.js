27d06c7d7ab56229f1ab5c6c74ae6496a3b17047
giggity-hub <imigstefan@gmail.com>
2021-09-30 18:15:03 +0200

    add download button

--- src/theme/Download/index.js
+++ src/theme/Download/index.js
@@ -1,8 +1,31 @@
 import React from 'react'
 import Link from '@docusaurus/Link';
 
+function fileNameFromPath(path){
+    return path.replace(/^.*[\\\/]/, '')
+}
+
 // path has to start in static
 export const DownloadLink = ({path, text, name}) => {
-    let fileName = name ? name : path.replace(/^.*[\\\/]/, '')
-    return <Link download={fileName} to={path} target="_blank">{text ? text : fileName }</Link>
+    let fileName = name ? name : fileNameFromPath(path)
+    return (
+        <Link download={fileName} to={path} target="_blank">
+            {text ? text : fileName }
+            <i class="fas fa-download margin-left--xs"></i>
+        </Link>
+    )
+}
+
+
+export const DownloadButton = ({path, text, name})=>{
+    let fileName = name ? name : fileNameFromPath(path)
+    return (
+        <Link download={fileName} to={path} target="_blank">
+            <button class="button button--outline button--primary padding-left--md margin-bottom--md">
+                <i class="fas fa-download margin-right--sm"></i>
+                {text ? text : fileName }
+            </button>
+        </Link>
+        
+    )
 }
