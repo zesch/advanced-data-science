b3952be84e4005672b858564231d6fa9a2450ece
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2021-09-30 08:41:09 +0200

    Update index.js

--- src/theme/Download/index.js
+++ src/theme/Download/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from '@docusaurus/Link';
 
 // path has to start in static
-export const DownloadLink = ({path}) => {
-    let fileName = path.replace(/^.*[\\\/]/, '')
-    return <Link download={fileName} to={path} target="_blank">{fileName}</Link>
+export const DownloadLink = ({path, text, name}) => {
+    let fileName = name ? name : path.replace(/^.*[\\\/]/, '')
+    return <Link download={fileName} to={path} target="_blank">{text ? text : fileName }</Link>
 }
